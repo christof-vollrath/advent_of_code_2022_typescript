d0460be125815f1b9e94aa8c9a753361999ed305
Christof Vollrath <christof-vollrath@axelspringer.com>
2022-12-01 23:03:34 +0100

    Day 01 solved

--- day01.spec.ts
+++ day01.spec.ts
@@ -7,39 +7,115 @@ export function readFileInput(path: string) {
     return readFileSync(path, 'utf8')
 }
 
-function readFileInputLines(path: string) {
-    const inputData = readFileInput(path)
-    return inputData.split('\n')
-}
 
+function parseNumberLines(input: string) {
+    const lines = input.split('\n')
+    return lines.map((line) => {
+        if (line.trim().length === 0) return null
+        else return parseInt(line)
+    })
+}
 function readFileInputNumbers(path: string) {
-    const inputLines = readFileInputLines(path)
-    return inputLines.map((line) => parseInt(line))
+    const inputLines = readFileInput(path)
+    return parseNumberLines(inputLines)
 }
 
-describe("Day 01 Part One", () =>{
+function sumCaloriesOfElves(calories: (number | null)[]) {
+    const result: number[] = []
+    let sum = 0
+    for (const cal of calories) {
+        if (cal === null) {
+            result.push(sum)
+            sum = 0
+        } else {
+            sum += cal
+        }
+    }
+    return result
+}
+
+function maxCalories(calories: number[]) {
+    return Math.max(...calories)
+}
+
+function max3Calories(calories: number[]) {
+    const sortedCalories = calories.sort((a, b) => b - a) // descending
+    return sortedCalories.splice(0, 3)
+}
+
+function sumCalories(calories: number[]) {
+    let sum = 0
+    for ( const c of calories) {
+        sum += c
+    }
+    return sum
+}
+
+describe("Day 01 Part One", () => {
+    const example =
+    `1000
+    2000
+    3000
+    
+    4000
+    
+    5000
+    6000
+    
+    7000
+    8000
+    9000
+    
+    10000
+    `
     describe("Example", () => {
-        it("should do something", () => {
+        const exampleNumbers = parseNumberLines(example);
+        it("should have parsed number lines", () => {
+            expect(exampleNumbers.length).toBe(15)
+            expect(exampleNumbers[0]).toBe(1000)
+            expect(exampleNumbers[13]).toBe(10000)
+            expect(exampleNumbers[14]).toBe(null)
         })
+        it("should have calculated the calories for each elf", () => {
+            const sum = sumCaloriesOfElves(exampleNumbers)
+            expect(sum).toEqual([6000, 4000, 11000, 24000, 10000])
+        });
+        it("should find the elf with the most calories", () => {
+            const max = maxCalories(sumCaloriesOfElves(exampleNumbers))
+            expect(max).toEqual(24000)
+        });
+        const max3 = max3Calories(sumCaloriesOfElves(exampleNumbers))
+        it("should have found the top three elves with the most calories", () => {
+            expect(max3).toEqual([24000, 11000, 10000])
+        });
+        it("should sum the top 3 calories", () => {
+            expect(sumCalories(max3)).toEqual(45000)
+        });
     })
     describe("Exercise", () => {
-        describe("Read file input", () => {
-            it ("Should read input", () => {
-                const inputString = readFileInput("inputDay01.txt")
-                expect(inputString.length).toBe(9821)
+        const inputNumbers = readFileInputNumbers("inputDay01.txt")
+        describe("Part 1", () => {
+            describe("Read file input", () => {
+                it ("should have read and parsed input numbers", () => {
+                    expect(inputNumbers.length).toBe(2238)
+                    expect(inputNumbers[0]).toBe(17998)
+                    expect(inputNumbers[2236]).toBe(13688)
+                    expect(inputNumbers[2237]).toBe(null)
+                })
             })
-            it ("Should read input lines", () => {
-                const inputLines = readFileInputLines("inputDay01.txt")
-                expect(inputLines.length).toBe(2000)
-            })
-            it ("Should read input numbers", () => {
-                const inputNumbers = readFileInputNumbers("inputDay01.txt")
-                expect(inputNumbers.length).toBe(2000)
-                expect(inputNumbers[0]).toBe(103)
+            describe("Find solution", () => {
+                it ("should find the solution", () => {
+                    const max = maxCalories(sumCaloriesOfElves(inputNumbers))
+                    expect(max).toEqual(67658)
+                })
             })
         })
-        describe("Find solution", () => {
-            it ("should have found the solution", () => {
+        describe("Part 2", () => {
+            describe("Find solution", () => {
+                const max3 = max3Calories(sumCaloriesOfElves(inputNumbers))
+                it ("should find the solution", () => {
+                    expect(sumCalories(max3)).toEqual(200158)
+                });
             })
         })
     })
