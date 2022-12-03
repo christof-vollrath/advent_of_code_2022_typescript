f6a85ba966064460c2861306c1c0b817ec342899
Christof Vollrath <christof-vollrath@axelspringer.com>
2022-12-03 18:58:27 +0100

    Day 03 solved

---
+++ day02.spec.ts
@@ -0,0 +1,170 @@
+// Advent of code 2022 - day 02
+
+import {readFileSync} from "fs";
+
+
+export function readFileInput(path: string) {
+    return readFileSync(path, 'utf8')
+}
+
+type RockPaperScissors = {
+    opponent: Choice;
+    myself: Choice
+}
+
+enum Choice {
+    A = "A",
+    B = "B",
+    C = "C",
+    X = "X",
+    Y = "Y",
+    Z = "Z",
+}
+
+enum RoundResult {
+    WIN, LOOSE, DRAW
+}
+
+function toChoice(str: string): Choice {
+    return Choice[str as keyof typeof Choice]
+}
+
+function parseRockPaperScissors(input: string): RockPaperScissors[] {
+    const lines = input.split('\n')
+    return lines.map((line) => line.trim())
+        .filter((line) => line.length > 0)
+        .map((line) => {
+        const parts = line.split(' ')
+        return { opponent: toChoice(parts[0]), myself: toChoice(parts[1]) }
+    })
+}
+
+function playRound(rockPaperScissor: {opponent: Choice; myself: Choice}) {
+    switch (rockPaperScissor.opponent) {
+        case Choice.A: if (rockPaperScissor.myself == Choice.X) return RoundResult.DRAW
+            else  if (rockPaperScissor.myself == Choice.Z) return RoundResult.LOOSE
+            break;
+        case Choice.B: if (rockPaperScissor.myself == Choice.Y) return RoundResult.DRAW
+            else  if (rockPaperScissor.myself == Choice.X) return RoundResult.LOOSE
+                break;
+        case Choice.C: if (rockPaperScissor.myself == Choice.Z) return RoundResult.DRAW
+            else  if (rockPaperScissor.myself == Choice.Y) return RoundResult.LOOSE
+                break;
+    }
+    return RoundResult.WIN
+}
+
+function calculateScore(rockPaperScissor: {opponent: Choice; myself: Choice}) {
+    let result = 0
+    switch (rockPaperScissor.myself) {
+        case Choice.X: result += 1; break
+        case Choice.Y: result += 2; break
+        case Choice.Z: result += 3; break
+    }
+    const roundResult = playRound(rockPaperScissor)
+    switch(roundResult) {
+        case RoundResult.DRAW: result += 3; break
+        case RoundResult.WIN: result += 6; break
+        case RoundResult.LOOSE: result += 0; break
+    }
+    return result;
+}
+
+function calculateScoreSum(rockPaperScissors: RockPaperScissors[]) {
+    return calculateSum(rockPaperScissors, calculateScore)
+
+}
+
+function findRightMove(rps: RockPaperScissors) {
+    var expectedResult;
+    switch(rps.myself) {
+        case Choice.X: expectedResult = RoundResult.LOOSE; break
+        case Choice.Y: expectedResult = RoundResult.DRAW; break
+        case Choice.Z: expectedResult = RoundResult.WIN; break
+    }
+    for (const move of [Choice.X, Choice.Y, Choice.Z]) {
+        if (playRound({ opponent: rps.opponent, myself: move}) === expectedResult) return move
+    }
+    throw new Error("No move found")
+}
+
+function calculateScore2(rps: {opponent: Choice; myself: Choice}) {
+    const myMove = findRightMove(rps)
+    return calculateScore({ opponent: rps.opponent, myself: myMove} )
+}
+
+export function calculateSum<T> (list: T[], f: ((t: T) => number)) {
+    let sum = 0;
+    for (const v of list) {
+        sum += f(v);
+    }
+    return sum
+}
+
+function calculateScoreSum2(rockPaperScissors: RockPaperScissors[]) {
+    return calculateSum(rockPaperScissors, calculateScore2)
+}
+
+
+describe("Day 2 Part One", () => {
+    const example = `
+        A Y
+        B X
+        C Z
+    `
+    describe("Example", () => {
+        const rockPaperScissors = parseRockPaperScissors(example);
+        it("should have parsed rock paper scissors", () => {
+            expect(rockPaperScissors.length).toBe(3)
+            expect(rockPaperScissors[0]).toStrictEqual({ opponent: Choice.A, myself: Choice.Y })
+            expect(rockPaperScissors[2]).toStrictEqual({ opponent: Choice.C, myself: Choice.Z })
+        })
+
+        it("should find winner", () => {
+            expect(playRound({ opponent: Choice.A, myself: Choice.Y })).toBe(RoundResult.WIN)
+            expect(playRound({ opponent: Choice.B, myself: Choice.Y })).toBe(RoundResult.DRAW)
+            expect(playRound({ opponent: Choice.C, myself: Choice.Y })).toBe(RoundResult.LOOSE)
+        });
+        it("should calculate score", () => {
+            expect(calculateScore({ opponent: Choice.A, myself: Choice.Y })).toBe(2 + 6)
+            expect(calculateScore({ opponent: Choice.B, myself: Choice.Y })).toBe(2 + 3)
+            expect(calculateScore({ opponent: Choice.C, myself: Choice.Y })).toBe(2)
+        });
+        it("should calculate score sum", () => {
+            expect(calculateScoreSum(rockPaperScissors)).toBe(15)
+        });
+        it("should calculate find right move", () => {
+            expect(findRightMove({ opponent: Choice.A, myself: Choice.Y })).toBe(Choice.X)
+            expect(findRightMove({ opponent: Choice.A, myself: Choice.X })).toBe(Choice.Z)
+            expect(findRightMove({ opponent: Choice.A, myself: Choice.Z })).toBe(Choice.Y)
+        })
+        it("should calculate score", () => {
+            expect(calculateScore2({ opponent: Choice.A, myself: Choice.Y })).toBe(1 + 3)
+            expect(calculateScore2({ opponent: Choice.B, myself: Choice.Y })).toBe(2 + 3)
+            expect(calculateScore2({ opponent: Choice.C, myself: Choice.Y })).toBe(3 + 3)
+        });
+        it("should calculate score sum 2", () => {
+            expect(calculateScoreSum2(rockPaperScissors)).toBe(12)
+        })
+    })
+
+    describe("Exercise", () => {
+        const input = readFileInput("inputDay02.txt")
+        const rockPaperScissors = parseRockPaperScissors(input);
+        it("should have parsed rock paper scissors", () => {
+            expect(rockPaperScissors.length).toBe(2500)
+            expect(rockPaperScissors[0]).toStrictEqual({ opponent: Choice.C, myself: Choice.Y })
+            expect(rockPaperScissors[2499]).toStrictEqual({ opponent: Choice.B, myself: Choice.Z })
+        })
+        describe("Part 1", () => {
+            describe("Find solution", () => {
+                expect(calculateScoreSum(rockPaperScissors)).toBe(13446)
+          })
+        })
+         describe("Part 2", () => {
+            it("should calculate score sum 2", () => {
+                expect(calculateScoreSum2(rockPaperScissors)).toBe(13509)
+            });
+         })
+    })
+})
---
+++ day03.spec.ts
@@ -0,0 +1,152 @@
+// Advent of code 2022 - day 03
+
+import {readFileSync} from "fs";
+import {calculateSum} from "./day02.spec";
+
+
+export function readFileInput(path: string) {
+    return readFileSync(path, 'utf8')
+}
+
+export function parseRucksacks(input: string) {
+    return input.split("\n").map(l => l.trim()).filter(l => l.length > 0)
+}
+
+
+function string2array(str: string) {
+    return str.split("");
+}
+
+function splitIntoHalves(strings: string[]) {
+    const half = strings.length / 2
+    return [strings.slice(0,half), strings.slice(half)]
+}
+
+function intersectionOfTwoArrays<T>(arrays: T[][]) {
+    const set = new Set(arrays[1])
+    return arrays[0].filter(x => set.has(x));
+}
+
+function letterValue(str: string) {
+    if ("a" <= str && str <= "z") return str.charCodeAt(0) - "a".charCodeAt(0) + 1
+    if ("A" <= str && str <= "Z") return str.charCodeAt(0) - "A".charCodeAt(0) + 27
+}
+
+function calculateRucksackPriority(string: string[]): number {
+    return letterValue(string[0])!;
+}
+
+function calculateRucksackPriorities(rucksacks: string[]) {
+    return rucksacks.map(r => calculateRucksackPriority(intersectionOfTwoArrays(splitIntoHalves(string2array(r)))));
+}
+
+export function calculateSumNumbers (list: number[]) {
+    return calculateSum(list, x => x)
+}
+
+function groupBy3(strings: string[]) {
+    const result: string[][] = []
+    let puf: string[] = []
+    for(const str of strings) {
+        if (puf.length == 3) {
+            result.push(puf)
+            puf = []
+        }
+        puf.push(str)
+    }
+    result.push(puf)
+    return result
+}
+
+function intersectionOfStrings(strings: string[]) {
+    var result: string[] | null = null
+    for (const str of strings) {
+        if (result === null) {
+            result = string2array(str)
+        } else {
+            result = intersectionOfTwoArrays([result, string2array(str)])
+        }
+    }
+    return result!
+}
+
+function calculateGroupPriorities(input: string[]) {
+    const groups = groupBy3(input)
+    return groups.map(group => {
+        const groupIntersection = intersectionOfStrings(group)
+        return calculateRucksackPriority(groupIntersection)
+    })
+}
+
+describe("Day 3 Part One", () => {
+    const example = `
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    `
+    describe("helpful functions", () => {
+        it("should convert a string to an array", () => {
+           expect(string2array("abc")).toStrictEqual(["a", "b", "c"])
+        });
+        it("should split array in halves", () => {
+            expect(splitIntoHalves(string2array("abcd"))).toStrictEqual([["a", "b"], ["c", "d"]])
+        });
+        it("should find intersection", () => {
+            expect(intersectionOfTwoArrays(splitIntoHalves(string2array("abcdcb")))).toStrictEqual(["b", "c"])
+        });
+        it("should find priority", () => {
+            expect(letterValue("a")).toStrictEqual(1)
+            expect(letterValue("B")).toStrictEqual(28)
+        });
+        it ("should groupy by 3", () => {
+            const simpleRucksacks = ["1", "2", "3", "4", "5", "6"]
+            expect(groupBy3(simpleRucksacks)).toStrictEqual([["1", "2", "3"], ["4", "5", "6"]])
+        })
+        it("should find intersection of three arrays", () => {
+            expect(intersectionOfStrings(["abcd", "dcb", "acd"])).toStrictEqual(["c", "d"])
+        });
+    })
+
+    describe("Example", () => {
+        const rucksacks = parseRucksacks(example);
+        it("should have parsed rucksacks", () => {
+            expect(rucksacks.length).toBe(6)
+        })
+        it("should calculate priority of rucksack", () => {
+            expect(calculateRucksackPriority(intersectionOfTwoArrays(splitIntoHalves(string2array(rucksacks[0]))))).toBe(16)
+        })
+        it("should calculate priorities of all rucksacks", () => {
+            expect(calculateRucksackPriorities(rucksacks)).toStrictEqual([16, 38, 42, 22, 20, 19])
+        })
+        it("should calculate sum of priorities of all rucksacks", () => {
+            expect(calculateSumNumbers(calculateRucksackPriorities(rucksacks))).toStrictEqual(157)
+        })
+        it("should calculate priorities of all groups", () => {
+            expect(calculateGroupPriorities(rucksacks)).toStrictEqual([18, 52])
+        })
+        it("should calculate sum of priorities of all groups", () => {
+            expect(calculateSumNumbers(calculateGroupPriorities(rucksacks))).toStrictEqual(70)
+        })
+    })
+
+    describe("Exercise", () => {
+        const input = readFileInput("inputDay03.txt")
+        const rucksacks = parseRucksacks(input);
+        it("should have parsed rucksacks", () => {
+            expect(rucksacks.length).toBe(300)
+        })
+        describe("Part 1", () => {
+            describe("Find solution", () => {
+                expect(calculateSumNumbers(calculateRucksackPriorities(rucksacks))).toBe(7872)
+          })
+        })
+        describe("Part 2", () => {
+            it("should calculate sum of groups", () => {
+                expect(calculateSumNumbers(calculateGroupPriorities(rucksacks))).toBe(2497)
+            });
+        })
+    })
+})
